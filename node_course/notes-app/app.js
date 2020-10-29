5f63216dfa203dd924370084f46e88023c4efce7
swachil.patel <swachil.patel@green-apex.com>
2020-10-29 16:16:09 +0530

    Validator package installed

---
+++ node_course/notes-app/app.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const name = 'Swachil';
+
+// If file doesn't exist it creates it, and if it exists then it rewrites that file.
+// fs.writeFileSync('notes.txt', 'This is sample file created in node.');
+
+// It appends content to end of file.
+// fs.appendFileSync('notes.txt', 'My name is Swachil.');
+
+// 
